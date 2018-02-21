04cae8f7552d31684ff43adfec50304f8496b124
featherstone <walter.featherstone@viavisolutions.com>
2018-02-21 09:33:07 +0000

    Added README & Jenkins schema validation script
    Change-Id: Ifc5254ff7621c19315ff939063d421d16cab537f
    Signed-off-by: featherstone <walter.featherstone@viavisolutions.com>

---
+++ .jenkins.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+specfiles=$(ls | egrep "^[^.]*Api.(json|yaml)")
+
+fres=0
+for i in $specfiles ; do
+    echo "-- Validating OpenAPI file $i..."
+    swagger-tools validate $i
+    res=$?
+    fres=$(($fres||$res))
+    echo -e "-- Validator returned $res.\n"
+done
+
+echo "-- Final validator returns $fres."
+
+exit $fres
